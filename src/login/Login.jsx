9cb62c22ac32734150586114972dfab0a7028a44
ikaronauta <jaortiz0496@misena.edu.co>
2022-10-24 22:34:49 -0500

    update

--- src/login/Login.jsx
+++ src/login/Login.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
-import { redirect } from "react-router-dom";
+import { Navigate, useNavigate } from "react-router-dom";
 
 // BPMCO_PORTAL
 // BPMCOInicio12345678.
@@ -10,21 +10,24 @@ export function Login() {
   const [login, setLogin] = useState(false);
   const [user, setUser] = useState("");
   const [pass, setPass] = useState("");
+
+  const navigate = useNavigate();
   
  const conexion = async () => {
    const url =
    `https://valoremanalitica.bpmco.co/login?user=${user}&pass=${pass}`;
    const result = await axios.get(url)
    const response = JSON.parse(result.data.slice(1, -1));
+   
 
    if (response.hasOwnProperty("ID_ESTADO")) {
     setLogin(true);
     console.log("OK");
-    <Redirect />
+    navigate("/home");
    } else {
     setLogin(false);
     console.log("NO");
-    redirect("/");
+    navigate("/");
    }
 
  };
